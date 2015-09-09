e557bdd95b7397188e492525c266870469e18567
MoonLht <lihaitao@lihaitaodeiMac.local>
2015-09-09 17:06:51 +0800

    添加 删除、增加功能

--- ReloadCell_Demo/ContactViewController.m
+++ ReloadCell_Demo/ContactViewController.m
@@ -9,10 +9,14 @@
 #import "ContactViewController.h"
 #import "Contact.h"
 #import "ContactGroup.h"
-@interface ContactViewController ()<UITableViewDataSource,UITableViewDelegate,UIAlertViewDelegate>{
+
+#define kSearchBarHeight 44
+@interface ContactViewController ()<UITableViewDataSource,UITableViewDelegate,UIAlertViewDelegate,UISearchBarDelegate>{
     UITableView *_tableView;
+    UISearchBar *_searchBar;
     NSMutableArray *_contacts;
     NSIndexPath *_selectedIndexPath;
+    BOOL _isInsert;
 }
 
 @end
@@ -22,53 +26,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initData];
-    
+    [self addSearchBar];
     _tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
     _tableView.dataSource = self;
     _tableView.delegate = self;
     [self.view addSubview:_tableView];
     
 }
-
--(void)initData{
-    _contacts = [[NSMutableArray alloc]init];
-    
-    Contact *contact1=[Contact initWithFirstName:@"Cui" andLastName:@"Kenshin" andPhoneNumber:@"18500131234"];
-    Contact *contact2=[Contact initWithFirstName:@"Cui" andLastName:@"Tom" andPhoneNumber:@"18500131237"];
-    ContactGroup *group1=[ContactGroup initWithName:@"C" andDetail:@"With names beginning with C" andContacts:[NSMutableArray arrayWithObjects:contact1,contact2, nil]];
-    [_contacts addObject:group1];
-    
-    
-    
-    Contact *contact3=[Contact initWithFirstName:@"Lee" andLastName:@"Terry" andPhoneNumber:@"18500131238"];
-    Contact *contact4=[Contact initWithFirstName:@"Lee" andLastName:@"Jack" andPhoneNumber:@"18500131239"];
-    Contact *contact5=[Contact initWithFirstName:@"黎" andLastName:@"Rose" andPhoneNumber:@"18500131240"];
-    ContactGroup *group2=[ContactGroup initWithName:@"L" andDetail:@"With names beginning with L" andContacts:[NSMutableArray arrayWithObjects:contact3,contact4,contact5, nil]];
-    [_contacts addObject:group2];
-    
-    
-    
-    Contact *contact6=[Contact initWithFirstName:@"Sun" andLastName:@"Kaoru" andPhoneNumber:@"18500131235"];
-    Contact *contact7=[Contact initWithFirstName:@"Sun" andLastName:@"Rosa" andPhoneNumber:@"18500131236"];
-    
-    ContactGroup *group3=[ContactGroup initWithName:@"S" andDetail:@"With names beginning with S" andContacts:[NSMutableArray arrayWithObjects:contact6,contact7, nil]];
-    [_contacts addObject:group3];
-    
-    
-    Contact *contact8=[Contact initWithFirstName:@"Wang" andLastName:@"Stephone" andPhoneNumber:@"18500131241"];
-    Contact *contact9=[Contact initWithFirstName:@"Wang" andLastName:@"Lucy" andPhoneNumber:@"18500131242"];
-    Contact *contact10=[Contact initWithFirstName:@"Wang" andLastName:@"Lily" andPhoneNumber:@"18500131243"];
-    Contact *contact11=[Contact initWithFirstName:@"Wang" andLastName:@"Emily" andPhoneNumber:@"18500131244"];
-    Contact *contact12=[Contact initWithFirstName:@"Wang" andLastName:@"Andy" andPhoneNumber:@"18500131245"];
-    ContactGroup *group4=[ContactGroup initWithName:@"W" andDetail:@"With names beginning with W" andContacts:[NSMutableArray arrayWithObjects:contact8,contact9,contact10,contact11,contact12, nil]];
-    [_contacts addObject:group4];
-    
-    
-    Contact *contact13=[Contact initWithFirstName:@"Zhang" andLastName:@"Joy" andPhoneNumber:@"18500131246"];
-    Contact *contact14=[Contact initWithFirstName:@"Zhang" andLastName:@"Vivan" andPhoneNumber:@"18500131247"];
-    Contact *contact15=[Contact initWithFirstName:@"Zhang" andLastName:@"Joyse" andPhoneNumber:@"18500131248"];
-    ContactGroup *group5=[ContactGroup initWithName:@"Z" andDetail:@"With names beginning with Z" andContacts:[NSMutableArray arrayWithObjects:contact13,contact14,contact15, nil]];
-    [_contacts addObject:group5];
+- (IBAction)popBack:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+- (IBAction)remove:(id)sender {
+    _isInsert = NO;
+    [_tableView setEditing:!_tableView.isEditing animated:YES];
+}
+- (IBAction)add:(id)sender {
+    _isInsert = YES;
+     [_tableView setEditing:!_tableView.isEditing animated:YES];
 }
 
 #pragma mark - 数据源方法
@@ -156,11 +130,43 @@
     return 45;
 }
 
+#pragma mark 取得当前操作状态，根据不同的状态左侧出现不同的操作按钮
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (_isInsert) {
+        return UITableViewCellEditingStyleInsert;
+    }
+    return UITableViewCellEditingStyleDelete;
+}
+
 //#pragma mark 设置尾部说明内容高度
 //-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
 //    return 40;
 //}
+#pragma mark 删除操作
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    ContactGroup *group = _contacts[indexPath.section];
+    Contact *contact = group.contacts[indexPath.row];
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [group.contacts removeObject:contact];
+        
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];
+        
+        if (group.contacts.count == 0) {
+            [_contacts removeObject:group];
+            [tableView reloadData];
+        }
+    }else if(editingStyle==UITableViewCellEditingStyleInsert){
+        Contact *newContact=[[Contact alloc]init];
+        newContact.firstName=@"first";
+        newContact.lastName=@"last";
+        newContact.phoneNumber=@"12345678901";
+        [group.contacts copy];
+        [group.contacts insertObject:newContact atIndex:indexPath.row];
+        [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];//注意这里没有使用reladData刷新
+    }
+}
 
+#pragma mark 选中单元格
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     _selectedIndexPath = indexPath;
     ContactGroup *group = _contacts[indexPath.section];
@@ -191,9 +197,57 @@
     NSLog(@"section:%li,switch:%i",(long)sw.tag, sw.on);
 }
 
+-(void)initData{
+    _contacts = [[NSMutableArray alloc]init];
+    
+    Contact *contact1=[Contact initWithFirstName:@"Cui" andLastName:@"Kenshin" andPhoneNumber:@"18500131234"];
+    Contact *contact2=[Contact initWithFirstName:@"Cui" andLastName:@"Tom" andPhoneNumber:@"18500131237"];
+    ContactGroup *group1=[ContactGroup initWithName:@"C" andDetail:@"With names beginning with C" andContacts:[NSMutableArray arrayWithObjects:contact1,contact2, nil]];
+    [_contacts addObject:group1];
+    
+    
+    
+    Contact *contact3=[Contact initWithFirstName:@"Lee" andLastName:@"Terry" andPhoneNumber:@"18500131238"];
+    Contact *contact4=[Contact initWithFirstName:@"Lee" andLastName:@"Jack" andPhoneNumber:@"18500131239"];
+    Contact *contact5=[Contact initWithFirstName:@"黎" andLastName:@"Rose" andPhoneNumber:@"18500131240"];
+    ContactGroup *group2=[ContactGroup initWithName:@"L" andDetail:@"With names beginning with L" andContacts:[NSMutableArray arrayWithObjects:contact3,contact4,contact5, nil]];
+    [_contacts addObject:group2];
+    
+    
+    
+    Contact *contact6=[Contact initWithFirstName:@"Sun" andLastName:@"Kaoru" andPhoneNumber:@"18500131235"];
+    Contact *contact7=[Contact initWithFirstName:@"Sun" andLastName:@"Rosa" andPhoneNumber:@"18500131236"];
+    
+    ContactGroup *group3=[ContactGroup initWithName:@"S" andDetail:@"With names beginning with S" andContacts:[NSMutableArray arrayWithObjects:contact6,contact7, nil]];
+    [_contacts addObject:group3];
+    
+    
+    Contact *contact8=[Contact initWithFirstName:@"Wang" andLastName:@"Stephone" andPhoneNumber:@"18500131241"];
+    Contact *contact9=[Contact initWithFirstName:@"Wang" andLastName:@"Lucy" andPhoneNumber:@"18500131242"];
+    Contact *contact10=[Contact initWithFirstName:@"Wang" andLastName:@"Lily" andPhoneNumber:@"18500131243"];
+    Contact *contact11=[Contact initWithFirstName:@"Wang" andLastName:@"Emily" andPhoneNumber:@"18500131244"];
+    Contact *contact12=[Contact initWithFirstName:@"Wang" andLastName:@"Andy" andPhoneNumber:@"18500131245"];
+    ContactGroup *group4=[ContactGroup initWithName:@"W" andDetail:@"With names beginning with W" andContacts:[NSMutableArray arrayWithObjects:contact8,contact9,contact10,contact11,contact12, nil]];
+    [_contacts addObject:group4];
+    
+    
+    Contact *contact13=[Contact initWithFirstName:@"Zhang" andLastName:@"Joy" andPhoneNumber:@"18500131246"];
+    Contact *contact14=[Contact initWithFirstName:@"Zhang" andLastName:@"Vivan" andPhoneNumber:@"18500131247"];
+    Contact *contact15=[Contact initWithFirstName:@"Zhang" andLastName:@"Joyse" andPhoneNumber:@"18500131248"];
+    ContactGroup *group5=[ContactGroup initWithName:@"Z" andDetail:@"With names beginning with Z" andContacts:[NSMutableArray arrayWithObjects:contact13,contact14,contact15, nil]];
+    [_contacts addObject:group5];
+}
 
 
-
+#pragma mark 添加搜索框
+-(void)addSearchBar{
+    CGRect searchBarRect = CGRectMake(0, 0, self.view.frame.size.width, kSearchBarHeight);
+    _searchBar = [[UISearchBar alloc]initWithFrame:searchBarRect];
+    _searchBar.placeholder = @"请在此处输入关键字...";
+    _searchBar.showsCancelButton = YES;
+    _searchBar.delegate = self;
+    _tableView.tableHeaderView = _searchBar;
+}
 
 
 
